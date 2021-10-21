7000ed967ed3af46ef921edb8b94cf4a6add7466
deigo-lps <diego.lopes0909@gmail.com>
2021-10-21 16:13:00 -0300

    remocao

--- hash.c
+++ hash.c
@@ -53,7 +53,7 @@ int main()
             table[h(num)]->ant = novo;
             table[h(num)] = novo;
 
-            printf("Inserido na posição %d.\n\n", h(num));
+            printf("Valor %d inserido na posição %d.\n\n",num, h(num));
           }
           else
             break;
@@ -65,10 +65,74 @@ int main()
           novo->valor = num;
           novo->prox = novo->ant = NULL;
           table[h(num)] = novo;
-          printf("Inserido posicao %d\n\n", h(num));
+          printf("Valor %d inserido posicao %d\n\n",num, h(num));
         }
         break;
       }
+
+      //remove da tabela
+      case 2:
+      {
+        int found=0;
+        printf("Digite o valor a ser removido: ");
+        scanf("%d",&num);
+        //se posição estiver vazia
+        if (table[h(num)] == NULL)
+        {
+          printf("Valor não encontrado.\n\n");
+          break;
+        }
+        //se primeiro valor da posição for o desejado
+        else if (table[h(num)]->valor == num)
+        {
+          printf("valor %d na posicao %d removido.\n\n",num,h(num));
+          //se for o único da posiçãp
+          if(table[h(num)]->prox==NULL)
+          {
+            free(table[h(num)]);
+            table[h(num)]=NULL;
+          }
+          //se não for o único
+          else
+          {
+            table[h(num)]=table[h(num)]->prox;
+            free(table[h(num)]->ant);
+            table[h(num)]->ant=NULL;
+          }
+          break;
+        }
+        //procura valor na posição
+        else
+        {
+          aux = table[h(num)];
+          do
+          { //se encontrou
+            if (aux->valor == num)
+            {
+              printf("Valor %d na posição %d removido.\n\n",num, h(num));
+              //se for o último
+              if(aux->prox==NULL){
+                aux->ant->prox=NULL;
+                free(aux);
+              }
+              //se não for o último
+              else{
+                aux->ant->prox=aux->prox;
+                aux->prox->ant=aux->ant;
+                free(aux);
+              }
+              found = 1;
+              break;
+            }
+            aux = aux->prox;
+          } while (aux != NULL);
+        }
+        //se o valor não foi encontrado
+        if (!found)
+          printf("Valor não encontrado.\n\n");
+        break;
+      }
+      
       //Encontra posição do valor na tabela.
       case 3:
       {
@@ -84,7 +148,7 @@ int main()
         //se primeiro elemento da posição for o procurado
         else if (table[h(num)]->valor == num)
         {
-          printf("Valor encontrado na posição %d.\n\n", h(num));
+          printf("Valor %d encontrado na posição %d.\n\n",num, h(num));
           break;
         }
         //se primeiro elemento não for o procurado
@@ -95,7 +159,7 @@ int main()
           {
             if (aux->valor == num)
             {
-              printf("Valor encontrado na posição %d.\n\n", h(num));
+              printf("Valor %d encontrado na posição %d.\n\n",num, h(num));
               found = 1;
               break;
             }
